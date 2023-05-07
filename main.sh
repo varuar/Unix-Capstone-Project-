ecf16568fb17d55fbad5a88767ec432c37c37ca2
varuar <varuar@farmingdale.edu>
2023-05-07 16:00:55 -0400

    Updated

--- main.sh
+++ main.sh
@@ -4,64 +4,73 @@ clear
 echo -e "\e[1;31mWelcome to the Hospital Patient Management Portal System!!!\e[0m"
 echo ""
 
-while true; do
-    echo "[L/l]List patients"
-    echo "[A/a]Add a new patient"
-    echo "[S/s]Search patient"
-    echo "[D/d]Delete patient"
-    echo "[E/e]Exit"
+
+while true
+do
+    echo "[L/l] List patients"
+    echo "[A/a] Add a new patient"
+    echo "[S/s] Search patient"
+    echo "[D/d] Delete patient"
+    echo "[E/e] Exit"
     echo ""
-    read -p "Enter your choice: " choice
+    read -p "Please select an option: " choice
     echo ""
-
-    if [[ $choice == "L" || $choice == "l" ]]; then
-        echo -e "\e[1;34mListing all patients...\e[0m"
-        echo ""
-        sort -t ',' -k 2,2 -k 3,3 patients.csv | awk '{print toupper($2), toupper($3), $4, $1}' | column -t -s $'\t'
-        echo ""
-    elif [[ $choice == "A" || $choice == "a" ]]; then
-        read -p "Enter first name: " fname
-        read -p "Enter last name: " lname
-        read -p "Enter phone number: " phone
-
-        id=$(echo "${lname:0:4}${fname:0:1}" | tr '[:lower:]' '[:upper:]')
-
-        while read line; do
-            if [[ $line == *"$id"* ]]; then
-                num=$(echo $line | awk -F',' '{print substr($1, 6)}')
-                if [[ -z $num ]]; then
-                    id="${id}1"
-                else
-                    id="${id}$(expr $num + 1)"
-                fi
-            fi
-        done < patients.csv
-
-        echo "The new Patient ID is $id"
-
-        echo "$id,$fname,$lname,$phone" >> patients.csv
-        echo "The new patient is added to the patient records."
-        echo ""
-    elif [[ $choice == "S" || $choice == "s" ]]; then
-        read -p "Enter the last name to search: " search
-        echo ""
-        echo "Here are the matching records..."
-        echo ""
-        grep -i "$search" patients.csv | awk -F',' '{print toupper($2), toupper($3), $4, $1}' | column -t -s $'\t'
-        echo ""
-    elif [[ $choice == "D" || $choice == "d" ]]; then
-        read -p "Enter the last name to delete: " delete
-        echo ""
-        grep -iv "$delete" patients.csv > patients_temp.csv
-        mv patients_temp.csv patients.csv
-        echo "Patient records matching \"$delete\" have been deleted."
-        echo ""
-    elif [[ $choice == "E" || $choice == "e" ]]; then
-        echo -e "\e[1;31mThanks for using our patient portal system!\e[0m"
-        echo ""
-        break
-    else
-        echo "Invalid choice, please try again."
-        echo ""
-    fi
+    
+    case $choice in
+        [Ll])
+            echo -e "\e[1;33mList of all patients:\e[0m"
+            echo ""
+            sort -t "," -k 2,2 -k 1,1 patients.csv | awk -F "," '{printf("%s\t%s\t%s\t%s\n", toupper($2), toupper($3), $4, $1)}'
+            echo ""
+            ;;
+        [Aa])
+            read -p "Enter first name: " first_name
+            read -p "Enter last name: " last_name
+            read -p "Enter phone number: " phone_number
+            
+         
+            last_name_prefix=$(echo $last_name | cut -c1-4 | tr '[:lower:]' '[:upper:]')
+            first_name_prefix=$(echo $first_name | cut -c1 | tr '[:lower:]' '[:upper:]')
+            patient_id="$last_name_prefix$first_name_prefix"
+            
+        
+            counter=2
+            while grep -q $patient_id patients.csv; do
+                patient_id="${last_name_prefix}${first_name_prefix}${counter}"
+                counter=$((counter+1))
+            done
+            
+            # Add new patient record to patients.csv
+            echo "$patient_id,$first_name,$last_name,$phone_number" >> patients.csv
+            
+            echo ""
+            echo -e "\e[1;33mThanks for entering the new patient information...\e[0m"
+            echo "The new Patient ID is $patient_id"
+            echo "The new patient is added to the patient records."
+            echo ""
+            ;;
+        [Ss])        
+            read -p "Enter the last name to search: " search_name
+            echo ""
+            echo "Here are the matching records..."
+            echo ""
+            grep -i "$search_name" patients.csv | awk -F "," '{printf("%s\t%s\t%s\t%s\n", toupper($3), toupper($2), $4, $1)}'
+            echo ""
+            ;;
+        [Dd])         
+            read -p "Enter the patient's last name or part of it to delete: " delete_name
+            echo ""
+            grep -v -i "$delete_name" patients.csv > temp.csv && mv temp.csv patients.csv
+            echo -e "\e[1;33mAll matching records have been deleted.\e[0m"
+            echo ""
+            ;;
+        [Ee])
+            echo -e "\e[1;32mThanks for using our patient portal system!\e[0m"
+            exit 0
+            ;;
+        *)
+            echo -e "\e[1;31mInvalid option. Please try again.\e[0m"
+            echo ""
+            ;;
+    esac
 done
