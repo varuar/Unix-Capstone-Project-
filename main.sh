f79baf13c985717c970201be910ad60775a2fe89
varuar <116020359+varuar@users.noreply.github.com>
2023-05-07 16:22:06 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,6 @@ clear
 echo -e "\e[1;31mWelcome to the Hospital Patient Management Portal System!!!\e[0m"
 echo ""
 
-
 while true
 do
     echo "[L/l] List patients"
@@ -27,20 +26,17 @@ do
             read -p "Enter first name: " first_name
             read -p "Enter last name: " last_name
             read -p "Enter phone number: " phone_number
-            
-         
+                   
             last_name_prefix=$(echo $last_name | cut -c1-4 | tr '[:lower:]' '[:upper:]')
             first_name_prefix=$(echo $first_name | cut -c1 | tr '[:lower:]' '[:upper:]')
             patient_id="$last_name_prefix$first_name_prefix"
             
-        
             counter=2
             while grep -q $patient_id patients.csv; do
                 patient_id="${last_name_prefix}${first_name_prefix}${counter}"
                 counter=$((counter+1))
             done
             
-  
             echo "$patient_id,$first_name,$last_name,$phone_number" >> patients.csv
             
             echo ""
